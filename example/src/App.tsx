b7cd2a35e9bdf460891b03e5d4e4fe1e2b9cff72
OscarDRT <oscarnetworkingpro@gmail.com>
2023-02-05 16:02:28 -0500

    feat: update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { normalize, normalizeHeight } from 'react-native-responsive-design';
+import { normalize, normalizeHeight } from 'rn-responsive-design';
 
 export default function App() {
   return (
